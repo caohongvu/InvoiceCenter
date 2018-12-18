afba001fd738462cbad63c2da179146e5466580c
Vincent Cao <caohongvu@gmail.com>
2018-12-18 10:13:25 +0700

    update mail

--- invoice-application/src/main/java/net/cis/service/impl/EmailServiceImpl.java
+++ invoice-application/src/main/java/net/cis/service/impl/EmailServiceImpl.java
@@ -1,8 +1,13 @@
 package net.cis.service.impl;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 import net.cis.service.EmailService;
@@ -13,19 +18,19 @@ import net.cis.service.EmailService;
 public class EmailServiceImpl implements EmailService {
 
 	@Autowired
-	MailSender mailSender;
+	JavaMailSender mailSender;
 	
 	@Override
 	public void send(String title, String content) {
-		SimpleMailMessage emailObj = new SimpleMailMessage();
-		emailObj.setFrom("automailer@cis.net.vn");
-		emailObj.setTo("operation_iparking@cis.net.vn");
-		emailObj.setSubject(title);
-		emailObj.setText(content);
-		mailSender.send(emailObj);
-		
+		mailSender.send(new MimeMessagePreparator() {
+			@Override
+			public void prepare(MimeMessage mimeMessage) throws Exception {
+				MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+				messageHelper.setFrom("automailer@cis.net.vn");
+				messageHelper.setTo("operation_iparking@cis.net.vn");
+				messageHelper.setSubject(title);
+				messageHelper.setText(content, true);
+			}
+		});
 	}
-
-	
-
 }
